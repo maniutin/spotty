81ef5eab0ffeb1a6a922716efba05f8e3d0073f4
Nick <maniutin@gmail.com>
2024-08-19 15:25:49 -0400

    display artist and title and minor styling

--- app/index.tsx
+++ app/index.tsx
@@ -1,66 +1,9 @@
-import React, { useState } from "react";
-import { Text, View, TouchableOpacity, Image, StyleSheet } from "react-native";
-import axios from "axios";
-import * as ImagePicker from "expo-image-picker";
-import * as FileSystem from "expo-file-system";
+import React from "react";
+import { View } from "react-native";
+
+import ImageAnalyzer from "@/components/ImageAnalyzer";
 
 export default function Index() {
-  const [imageUri, setImageUri] = useState("");
-  const [labels, setLabels] = useState([]);
-
-  const pickImage = async () => {
-    try {
-      let result = await ImagePicker.launchImageLibraryAsync({
-        mediaTypes: ImagePicker.MediaTypeOptions.Images,
-        allowsEditing: true,
-        aspect: [4, 3],
-        quality: 1,
-      });
-
-      if (!result.canceled) {
-        setImageUri(result.assets[0].uri);
-      }
-      console.log(result);
-    } catch (error) {
-      console.error("Error picking image:", error);
-    }
-  };
-
-  const analyzeImage = async () => {
-    try {
-      if (!imageUri) {
-        alert("Please select an image first.");
-        return;
-      }
-
-      // Replace 'YOUR_GOOGLE_CLOUD_VISION_API_KEY' with your actual API key
-      const apiKey = process.env.EXPO_PUBLIC_VISION_API_KEY;
-      const apiUrl = `https://vision.googleapis.com/v1/images:annotate?key=${apiKey}`;
-
-      // Read the image file from local URI and convert it to base64
-      const base64ImageData = await FileSystem.readAsStringAsync(imageUri, {
-        encoding: FileSystem.EncodingType.Base64,
-      });
-
-      const requestData = {
-        requests: [
-          {
-            image: {
-              content: base64ImageData,
-            },
-            features: [{ type: "LABEL_DETECTION", maxResults: 5 }],
-          },
-        ],
-      };
-
-      const apiResponse = await axios.post(apiUrl, requestData);
-      setLabels(apiResponse.data.responses[0].labelAnnotations);
-    } catch (error) {
-      console.error("Error analyzing image:", error);
-      alert("Error analyzing image. Please try again later.");
-    }
-  };
-
   return (
     <View
       style={{
@@ -69,24 +12,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      {imageUri && (
-        <Image source={{ uri: imageUri }} style={{ width: 300, height: 300 }} />
-      )}
-      <TouchableOpacity onPress={pickImage}>
-        <Text>Pick Image</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={analyzeImage}>
-        <Text>Analyze Image</Text>
-      </TouchableOpacity>
-      {labels.length > 0 && (
-        <View>
-          <Text>Labels:</Text>
-          {labels.map((label) => (
-            <Text key={label.mid}>{label.description}</Text>
-          ))}
-        </View>
-      )}
+      <ImageAnalyzer />
     </View>
   );
 }
---
+++ components/ImageAnalyzer.tsx
@@ -0,0 +1,127 @@
+import React, { useState } from "react";
+import { Text, View, TouchableOpacity, Image, StyleSheet } from "react-native";
+import axios from "axios";
+import * as ImagePicker from "expo-image-picker";
+import * as FileSystem from "expo-file-system";
+
+export default function ImageAnalyzer() {
+  const [imageUri, setImageUri] = useState("");
+  const [labels, setLabels] = useState([]);
+  const [artist, setArtist] = useState("");
+  const [title, setTitle] = useState("");
+
+  const pickImage = async () => {
+    try {
+      let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+      });
+
+      if (!result.canceled) {
+        setImageUri(result.assets[0].uri);
+      }
+      console.log(result);
+    } catch (error) {
+      console.error("Error picking image:", error);
+    }
+  };
+
+  const analyzeImage = async () => {
+    try {
+      if (!imageUri) {
+        alert("Please select an image first.");
+        return;
+      }
+
+      // Replace 'YOUR_GOOGLE_CLOUD_VISION_API_KEY' with your actual API key
+      const apiKey = process.env.EXPO_PUBLIC_VISION_API_KEY;
+      const apiUrl = `https://vision.googleapis.com/v1/images:annotate?key=${apiKey}`;
+
+      // Read the image file from local URI and convert it to base64
+      const base64ImageData = await FileSystem.readAsStringAsync(imageUri, {
+        encoding: FileSystem.EncodingType.Base64,
+      });
+
+      const requestData = {
+        requests: [
+          {
+            image: {
+              content: base64ImageData,
+            },
+            features: [{ type: "WEB_DETECTION", maxResults: 5 }],
+          },
+        ],
+      };
+
+      const apiResponse = await axios.post(apiUrl, requestData);
+      // setLabels(
+      //   apiResponse.data.responses[0].webDetection.bestGuessLabels[0].label
+      // );
+      setArtist(
+        apiResponse.data.responses[0].webDetection.webEntities[0].description
+      );
+      setTitle(
+        apiResponse.data.responses[0].webDetection.webEntities[1].description
+      );
+      console.log(
+        "=====",
+        JSON.stringify(apiResponse.data.responses[0].webDetection.webEntities)
+      );
+    } catch (error) {
+      console.error("Error analyzing image:", error);
+      alert("Error analyzing image. Please try again later.");
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>
+        Select an image from Library and click "Analyze Image"
+      </Text>
+      {imageUri && (
+        <Image source={{ uri: imageUri }} style={{ width: 300, height: 300 }} />
+      )}
+      <TouchableOpacity onPress={pickImage} style={styles.button}>
+        <Text style={styles.text}>Pick Image</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={analyzeImage} style={styles.button}>
+        <Text style={styles.text}>Analyze Image</Text>
+      </TouchableOpacity>
+      <Text style={styles.label}>
+        {artist || title ? `${artist} ${title}` : "can't find it"}
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    textAlign: "center",
+    fontSize: 30,
+    fontWeight: "bold",
+    marginBottom: 50,
+    marginTop: 100,
+  },
+  button: {
+    backgroundColor: "#dddddd",
+    padding: 10,
+    marginBottom: 10,
+    marginTop: 20,
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  label: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginTop: 20,
+  },
+});
